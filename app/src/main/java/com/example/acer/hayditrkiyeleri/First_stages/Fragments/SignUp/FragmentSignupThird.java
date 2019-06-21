a0d87d26b5dee2597c10ca372df8d1bb8c5df360
keremeyuboglu <32223948+keremeyuboglu@users.noreply.github.com>
2019-06-21 16:42:35 +0300

    Deneme eklenin oraya scroll atıldı da dinamik değişme yapılacak

--- app/src/main/java/com/example/acer/hayditrkiyeleri/First_stages/Fragments/SignUp/FragmentSignupThird.java
+++ app/src/main/java/com/example/acer/hayditrkiyeleri/First_stages/Fragments/SignUp/FragmentSignupThird.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -213,6 +214,7 @@ public class FragmentSignupThird extends Fragment {
             }
         }).attachToRecyclerView(recyclerView);
 
+        ScrollView mScroll = v.findViewById(R.id.scroll_recycler);
 
         return v;
 
