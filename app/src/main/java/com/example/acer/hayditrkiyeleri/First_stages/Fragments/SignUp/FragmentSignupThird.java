aa111ad7c91c3a9acf64df8840049eb4e3e6ad6b
seko <aserkangoktas@gmail.com>
2019-06-25 20:04:48 +0300

    uygulamayı bozuyor

--- app/src/main/java/com/example/acer/hayditrkiyeleri/First_stages/Fragments/SignUp/FragmentSignupThird.java
+++ app/src/main/java/com/example/acer/hayditrkiyeleri/First_stages/Fragments/SignUp/FragmentSignupThird.java
@@ -2,7 +2,6 @@ package com.example.acer.hayditrkiyeleri.First_stages.Fragments.SignUp;
 
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
